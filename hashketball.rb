c261561ed492285771c3e77706094cf1e86e2bd8
Christopher Demahy <demahyc@gmail.com>
2020-10-01 15:38:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -83,7 +83,18 @@ def game_hash
         steals: 22,
         blocks: 15,
         slam_dunks: 10,
-      }
+      },
+      {
+        player_name: "DaSagna Diop",
+        number: 2,
+        shoe: 14,
+        points: 24,
+        rebounds: 12,
+        assists: 12,
+        steals: 4,
+        blocks: 5,
+        slam_dunks: 5,
+        }
       ]
     }
   }
