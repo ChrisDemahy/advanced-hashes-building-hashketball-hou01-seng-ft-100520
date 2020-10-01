36dba6ab53a442255a6441fa37e0c6549dfab73e
Christopher Demahy <demahyc@gmail.com>
2020-10-01 15:21:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,15 @@
 # Returns a Nested Data structure containing data from the NBA
 def game_hash
   # Return the hash
-  {}
+  {
+    home: {
+      team_name: "",
+      colors: [""],
+      players: [{}]
+    },
+    away: {
+      
+    }
+  }
 
 end
